fea15ab11e8e7f505ea45bf15464fc472e272fc4
Said <shahrokh07.96@gmail.com>
2023-04-22 10:41:57 +0530

    easybank landing page initial commit

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+const btnHamburger = document.querySelector("#btn-hamburger");
+const header = document.querySelector('#header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+btnHamburger.addEventListener('click', function() {
+    
+    if(header.classList.contains('open')) {
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else {
+        body.classList.add('no-scroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
+
